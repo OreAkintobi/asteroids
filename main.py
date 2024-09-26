a99b6c6c4a4aead83b9e3b9f970f5fcb4f2c5dc5
Ore Akintobi <oreakintobi@gmail.com>
2024-09-26 20:45:02 +0200

    A: initial commit - pygame screen painted

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+# import constants
+from constants import *
+
+def main() : 
+	pygame.init()
+	print("Starting asteroids!")
+
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		
+		screen.fill("black", rect=None, special_flags=0)
+		pygame.display.flip()
+
+	
+if __name__ == "__main__":
+	main()
